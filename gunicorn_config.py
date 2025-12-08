2ea57eb7a827621a7720255ca5a8fee176d65581
bhatt.p <bhatt.p@internal.media.net>
2025-12-09 00:51:26 +0530

    Jai Shri Ram

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,4 +5,4 @@ keepalive = 5
 max_requests = 100  # Reduced from 1000
 max_requests_jitter = 10
 graceful_timeout = 60
-worker_tmp_dir = '/dev/shm'  # Use RAM for temp files
+# worker_tmp_dir = '/dev/shm'  # Use RAM for temp files
