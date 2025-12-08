98a3123073d3da0c9ce342f4196fa1c07b806c58
bhatt.p <bhatt.p@internal.media.net>
2025-12-09 00:24:33 +0530

    Jai Shri Ram

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,8 @@
 workers = 1
 worker_class = 'sync'
-timeout = 120
+timeout = 300  # Increased from 120
 keepalive = 5
-max_requests = 1000
-max_requests_jitter = 50
-graceful_timeout = 30
+max_requests = 100  # Reduced from 1000
+max_requests_jitter = 10
+graceful_timeout = 60
+worker_tmp_dir = '/dev/shm'  # Use RAM for temp files
